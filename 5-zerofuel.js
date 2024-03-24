0ac5dca7b447ec13bb14a26c8bddf032b0bb3f2e
haedar05 <m.haedaramin@gmail.com>
2024-03-24 14:31:45 +0800

    Day 5/366

---
+++ 5-zerofuel.js
@@ -0,0 +1,16 @@
+// WPU Coding Challenge 2024
+// 5 / 366;
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return distanceToPump / mpg <= fuelLeft;
+//   };
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return fuelLeft * mpg >= distanceToPump;
+//   };
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
+  fuelLeft * mpg >= distanceToPump;
+
+console.log(zeroFuel(50, 25, 2));
