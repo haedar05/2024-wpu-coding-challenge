affa11841f4e30789868fb19c7e662ad10c4ae6a
haedar05 <m.haedaramin@gmail.com>
2024-03-29 05:53:29 +0800

    day 14/366

---
+++ 14-getGrade.js
@@ -0,0 +1,22 @@
+// WPU Coding Challenge 2024
+// 14/ 366;
+// https://www.codewars.com/kata/55cbd4ba903825f7970000f5
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+function getGrade(...s) {
+  const score = s.reduce((acc, curr) => acc + curr) / s.length;
+  if (score < 60) return "F";
+  if (score < 70) return "D";
+  if (score < 80) return "C";
+  if (score < 90) return "B";
+  return "A";
+}
+
+console.log(getGrade(65, 70, 59));
